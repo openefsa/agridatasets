21592646538ecb2352a348bccbcefaa92e231ee8
Pierre Casadebaig <pierre.casadebaig@inra.fr>
2018-01-19 16:49:08 +0100

    corrected a bug in distance_haversine (causing NaN distance and subsequent errors when ranking stations)

--- R/tools.r
+++ R/tools.r
@@ -1,14 +1,14 @@
-# Compute distance between two points: http://en.wikipedia.org/wiki/Haversine_formula
+# Compute distance in km between two points: http://en.wikipedia.org/wiki/Haversine_formula
 #' @export distance_haversine
 
-distance_haversine <- function(lat1, lon1, lat2, lon2) {
-  
-  # Earth radius in km
-  R <- 6371
-  delta_lon <- (lon2*pi/180 - lon1*pi/180) # decimal degrees to radians
-  delta_lat <- (lat2*pi/180 - lat1*pi/180) # decimal degrees to radians
-  a <- sin(delta_lat/2)^2 + cos(lat1) * cos(lat2) * sin(delta_lon/2)^2
-  c <- 2 * asin(pmin(1,sqrt(a)))
-  d = R * c # distance in km
-  return(d)
+distance_haversine <- function(lat1, lon1, lat2, lon2, r = 6378.137){
+  radians <- pi/180
+  lat2 <- lat2 * radians
+  lat1 <- lat1 * radians
+  lon2 <- lon2 * radians
+  lon1 <- lon1 * radians
+  dLat <- (lat2 - lat1)
+  dLon <- (lon2 - lon1)
+  a <- (sin(dLat/2)^2) + (cos(lat1) * cos(lat2)) * (sin(dLon/2)^2)
+  return(2 * atan2(sqrt(a), sqrt(1 - a)) * r)
 }
